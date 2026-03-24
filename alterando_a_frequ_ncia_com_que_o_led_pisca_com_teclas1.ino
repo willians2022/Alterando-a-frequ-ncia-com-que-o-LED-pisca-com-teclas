b1985a7108b2fd86722f5c18f97a12dd7442c389
willians <106207723+willians2022@users.noreply.github.com>
2026-03-23 23:05:28 -0300

    Update alterando_a_frequ_ncia_com_que_o_led_pisca_com_teclas1.ino

--- alterando_a_frequ_ncia_com_que_o_led_pisca_com_teclas1.ino
+++ alterando_a_frequ_ncia_com_que_o_led_pisca_com_teclas1.ino
@@ -40,3 +40,85 @@ void loop (){
     }
   }
 }
+
+
+// ===== versão comentado ===== //
+
+/*
+//// Define o pino 2 como LED vermelho
+  #define Led_vermelho 2 
+
+// Define o pino 3 como LED amarelo
+#define Led_amarelo 3
+
+// Define o pino 4 como LED verde
+#define Led_verde 4
+
+// Variável para armazenar o comando recebido pela serial
+char opcao;
+
+// Função executada uma vez ao iniciar o Arduino
+void setup (){
+
+  // Define o pino do LED verde como saída
+  pinMode(Led_verde, OUTPUT);
+
+  // Define o pino do LED amarelo como saída
+  pinMode(Led_amarelo, OUTPUT);
+
+  // Define o pino do LED vermelho como saída
+  pinMode(Led_vermelho,OUTPUT);
+
+  // Inicia a comunicação serial com velocidade de 9600 bps
+  Serial.begin(9600);
+}
+
+// Função que roda continuamente
+void loop (){
+
+  // Verifica se existe algum dado disponível na serial
+  if(Serial.available()){
+
+    // Lê o caractere enviado pelo usuário
+    opcao = Serial.read();
+    
+    // Estrutura para verificar qual comando foi enviado
+    switch(opcao){
+
+      // Se receber 'G', liga o LED verde
+      case 'G':
+      digitalWrite (Led_verde, HIGH);
+      break;
+
+      // Se receber 'g', desliga o LED verde
+      case 'g':
+      digitalWrite (Led_verde, LOW);
+      break;
+      
+      // Se receber 'Y', liga o LED amarelo
+      case 'Y':
+      digitalWrite (Led_amarelo, HIGH);
+      break;
+
+      // Se receber 'y', desliga o LED amarelo
+      case 'y':
+      digitalWrite (Led_amarelo, LOW);
+      break;
+      
+      // Se receber 'R', liga o LED vermelho
+      case 'R':
+      digitalWrite (Led_vermelho, HIGH);
+      break;
+
+      // Se receber 'r', desliga o LED vermelho
+      case 'r':
+      digitalWrite (Led_vermelho, LOW);
+      break;
+      
+      // Caso o comando não seja reconhecido
+      default:
+      Serial.println ("Opcao Invalida");
+    }
+  }
+}
+*/
